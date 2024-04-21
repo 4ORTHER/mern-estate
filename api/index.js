cf9bc94da1cfd35db9fe04dd428fc531c55a3015
4ORTHER <Repfi.illy@gmail.com>
2024-04-21 18:52:49 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
-// import express from "express";
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Connected to MongoDB!");
+}).catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
 });
