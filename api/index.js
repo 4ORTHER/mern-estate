90a0f51bb3979e6bffaff9c4c6f3ea6606c7f4ce
4ORTHER <Repfi.illy@gmail.com>
2024-04-21 19:50:53 +0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user.route");
+const authRouter = require("./routes/auth.route")
 dotenv.config();
 
 mongoose
@@ -15,8 +16,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
